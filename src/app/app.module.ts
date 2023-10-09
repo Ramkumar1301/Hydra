b8c0a1db2e15bad430770cd41ea55e29672c7ac5
Ramkumar1301 <sairamprint.119@gmail.com>
2023-10-09 20:45:19 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { HeroSectionComponent } from './component/hero-section/hero-section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeroSectionComponent
   ],
   imports: [
     BrowserModule
